cc945044758386842747d22bbbfa20202c62124e
tg <tg@Stannas-MacBook-Air.local>
2014-03-28 09:34:24 +0700

    Commit with OSX 10 direct from Xcode 5.1

---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by tg on 3/28/14.
+//  Copyright (c) 2014 tgalbraith. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
