811c49eb9b00512d8b3df471a0f75f56ca638e12
tgalbraith <tg@Stannas-MacBook-Air.local>
2014-03-28 10:14:05 +0700

    test again

---
+++ Git Test/CCAnotherTest.h
@@ -0,0 +1,13 @@
+//
+//  CCAnotherTest.h
+//  Git Test
+//
+//  Created by tg on 3/28/14.
+//  Copyright (c) 2014 tgalbraith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCAnotherTest : NSObject
+
+@end
---
+++ Git Test/CCAnotherTest.m
@@ -0,0 +1,13 @@
+//
+//  CCAnotherTest.m
+//  Git Test
+//
+//  Created by tg on 3/28/14.
+//  Copyright (c) 2014 tgalbraith. All rights reserved.
+//
+
+#import "CCAnotherTest.h"
+
+@implementation CCAnotherTest
+
+@end
