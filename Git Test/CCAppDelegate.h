30bb3d09dc8c240f48c344ef52d5326c2231daa6
tg <tg@Stannas-MacBook-Air.local>
2014-03-28 09:05:58 +0700

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by tg on 3/28/14.
+//  Copyright (c) 2014 tgalbraith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
